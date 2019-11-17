72cc692d5ac80801121507ae3d0dd071c06b6d7d
Zia pmr <gernersnc@gmail.com>
2019-11-17 11:29:38 -0500

    Exercise 1, 2

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,8 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    if (n < 2) {
+        return n
+    }
+    return fib(n - 1) + fib(n - 2)
 }
--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -2,6 +2,19 @@
 Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
+
+var array = [6,2,3,7,5,4,1];
 function bubbleSort(numArray) {
-  
+    for(let j=0;j<numArray.length;j++) {
+        for(let i = 0; i < numArray.length; i++) {
+            if(numArray[i]>numArray[i+1]) {
+                var temp = numArray[i];
+                numArray[i] = [i+1];
+                numArray[i+1] = temp;
+            }
+        }
+    }      
+    return numArray;
 }
+console.log(bubbleSort(array));
+
